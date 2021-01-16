0184f964b9475b31da0dd0265dfc2f00e07b2db1
Hadi - WarungUmat <hadisetiawan1992@gmail.com>
2021-01-17 01:04:01 +0700

    First Commit

---
+++ models/users.go
@@ -0,0 +1,18 @@
+package models
+ 
+import (
+    "time"
+)
+ 
+type (
+    // User
+    User struct {
+        ID        		int       `json:"id"`
+        Email       	int       `json:"email"`
+        Nama_lengkap   	string    `name:"nama_lengkap"`
+        Semester  		int       `json:"semester"`
+        CreatedAt time.Time `json:"created_at"`
+        UpdatedAt time.Time `json:"updated_at"`
+    }
+)
+ 
